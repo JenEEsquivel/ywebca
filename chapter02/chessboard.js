2db076e24f420c46d0ca1c7282443d85f8784990
Jen <JenEEsquivel@gmail.com>
2015-07-24 11:04:22 -0500

    chessboard not yet finished

--- chapter02/chessboard.js
+++ chapter02/chessboard.js
@@ -17,6 +17,24 @@ Chess.start = function (n) {
 };
 
 
+/* this is your second solution, solving the ReadMe extra challenge. Finally,
+there is an extra credit test, above:
+var sz = n || 8;
+  var chessboard = "";
+  for (var a = 0; a < sz; a++) {  
+    for (var b = 0; b < sz; b++) {    
+    if ((a + b) % 2 == 0)
+    chessboard += " ";    
+    else chessboard += "#";  
+    }  
+    chessboard += "\n";
+  }
+    return chessboard;
+*/
+
+
+
+
 /* this was your first solution after finishing the exercise only from
 "Eloquent Javascript" at the end of chapter two. Next step above includes extra 
 challenges from the readme file.
